e455154284b44cdcd27ee09ba09301813e41a434
marodi-mykhailo <mykhailo.marodi@gmail.com>
2021-01-08 20:20:55 +0100

    add sagas for products

---
+++ src/api/api.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const settings = {}
+
+const instance = axios.create({
+    baseURL: 'http://produkty-uslugi.herokuapp.com/',
+    ...settings
+})
+
+export const productAPI = {
+    getAllProducts() {
+        return instance.get('products')
+    },
+    getProductById(id) {
+        return instance.get(`products/${id}`)
+    },
+    getProductsWithNipDiscount(nip) {
+        return instance.get(`products/${nip}`)
+    },
+    deleteProductById(id) {
+        return instance.delete(`products/${id}`)
+    },
+    createProduct(productData) {
+        return instance.post('products', productData)
+    },
+    updateProduct(id, productData) {
+        return instance.put(`products/${id}`, productData)
+    }
+}
---
+++ src/saga/products.saga.js
@@ -0,0 +1,12 @@
+import {call, put, takeEvery} from "@redux-saga/core/effects";
+import {productAPI} from "../api/api";
+import {setProducts} from "../redux/reducers/productsReducer";
+
+function* workerProductLoadData() {
+    const productsData = yield call(productAPI.getAllProducts)
+    yield put(setProducts(productsData))
+}
+
+export function* watchProductLoadData() {
+    yield takeEvery("LOAD_PRODUCTS", workerProductLoadData)
+}
